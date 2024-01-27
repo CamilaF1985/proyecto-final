8d1a5935fc54fca45da4dbfcc29fa7060f2908ec
Camila Fabbroni <camila.fabbroni1985@gmail.com>
2024-01-27 15:29:28 -0300

    recuperacion de registro

--- src/components/RegistroForm.jsx
+++ src/components/RegistroForm.jsx
@@ -11,86 +11,113 @@ import {
 } from '../flux/addressActions';
 import Swal from 'sweetalert2';
 import '../assets/css/App.css';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import format from "../assets/js/format";
-import validate from '../assets/js/validate';
 
 const RegistroForm = () => {
+  // Hooks y selectores
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const regiones = useSelector((state) => state.regiones);
   const comunas = useSelector((state) => state.comunas);
 
-  const handleRegresarClick = () => {
-    // Navegar a la ruta /
-    navigate('/');
-  };
-
   useEffect(() => {
+    // Cargar las regiones al montar el componente
     dispatch(fetchAllRegiones());
   }, [dispatch]);
 
-  const formik = useFormik({
-    initialValues: {
-      rut: '',
-      nombreUnidad: '',
-      idRegion: '',
+  console.log('Regiones:', regiones);
+
+  // Declarar el estado del formulario, inicializarr las variables en null
+  const [formData, setFormData] = useState({
+    rut: '',
+    email: '',
+    nombre: '',
+    contrasena: '',
+    nombreUnidad: '',
+    idRegion: '',
+    idComuna: '',
+    calle: '',
+    numero: '',
+    deptoCasa: '',
+  });
+
+  const handleChange = (e) => {
+    // Manejo de cambios en el formulario
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleRegionChange = (e) => {
+    // Manejo de cambios al seleccionar una región
+    const selectedRegionId = e.target.value;
+    setFormData({
+      ...formData,
+      idRegion: selectedRegionId,
       idComuna: '',
-      calle: '',
-      numero: '',
-      deptoCasa: '',
-      email: '',
-      nombre: '',
-      contrasena: ''
-    },
-    validationSchema: Yup.object({
-      rut: Yup.string()
-        .test('valida-rut', 'RUT inválido', function (value) {
-          return validate.rut(value);
-        })
-        .required('Requerido'),
-      nombreUnidad: Yup.string().required('Requerido'),
-      idRegion: Yup.string().notRequired(),
-      idComuna: Yup.string().notRequired(),
-      calle: Yup.string().required('Requerido'),
-      numero: Yup.string().notRequired(),
-      deptoCasa: Yup.string().notRequired(),
-      email: Yup.string().required('Requerido'),
-      nombre: Yup.string().required('Requerido'),
-      contrasena: Yup.string().required('Requerido')
-    }),
-    onSubmit: values => {
+    });
+
+    console.log('ID de Región:', selectedRegionId);
+    // Cargar las comunas para la región seleccionada
+    dispatch(fetchComunasByRegionId(selectedRegionId));
+  };
+
+  const handleComunaChange = (e) => {
+    // Carga las comunas para la región seleccionada
+    const selectedComunaId = e.target.value;
+    setFormData({
+      ...formData,
+      idComuna: selectedComunaId,
+    });
+
+    //Console.log para el id de la comuna seleccionada
+    console.log('ID de Comuna:', selectedComunaId);
+  };
+
+  // No enviar el formulario hasta que este completo
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Guardar los datos de la unidad en el estado global
       const unitData = {
-        nombre: values.nombreUnidad,
+        nombre: formData.nombreUnidad,
       };
 
+      // Llamar a la acción y trabajar con la promesa resultante usando then y catch
       dispatch(saveNewUnitData(unitData))
         .then((unitId) => {
           if (unitId) {
+            // Guardar la dirección en la base de datos
             const direccionData = {
               id_pais: '1',
-              id_region: values.idRegion,
-              id_comuna: values.idComuna,
-              calle: values.calle,
-              numero: values.numero,
-              depto_casa: values.deptoCasa,
+              id_region: formData.idRegion,
+              id_comuna: formData.idComuna,
+              calle: formData.calle,
+              numero: formData.numero,
+              depto_casa: formData.deptoCasa,
               id_unidad: unitId,
             };
 
+            console.log('Datos de la dirección:', direccionData);
+
             dispatch(createDireccionDB(direccionData))
               .then(() => {
+                // Si la creación de la unidad y dirección es exitosa, proceder a guardar el usuario
                 const userData = {
-                  rut: values.rut,
-                  email: values.email,
-                  nombre: values.nombre,
-                  contrasena: values.contrasena,
+                  rut: formData.rut,
+                  email: formData.email,
+                  nombre: formData.nombre,
+                  contrasena: formData.contrasena,
                   id_unidad: unitId,
                 };
 
                 dispatch(saveNewUserData(userData))
                   .then(() => {
+                    // Redirigir a la página principal después de completar el proceso de registro
                     navigate('/');
+
+                    // Mostrar mensaje de registro exitoso
                     Swal.fire({
                       icon: 'success',
                       title: '¡Registro Exitoso!',
@@ -99,6 +126,7 @@ const RegistroForm = () => {
                   })
                   .catch((error) => {
                     console.error('Error al guardar el usuario:', error);
+                    // Mostrar mensaje de error en el registro
                     Swal.fire({
                       icon: 'error',
                       title: '¡Ocurrió un error en el registro!',
@@ -116,9 +144,15 @@ const RegistroForm = () => {
         .catch((error) => {
           console.error('Error al guardar la unidad:', error);
         });
-    },
-  });
+    } catch (error) {
+      console.error('Error general:', error);
+    }
+  };
 
+  const handleRegresarClick = () => {
+    // Navegar a la ruta /
+    navigate('/');
+  };
 
   return (
     <div className="contenedor mt-4 mb-1 p-3 formulario-registro">
@@ -131,145 +165,73 @@ const RegistroForm = () => {
               <h2 className="form-titulo-registro">Registro</h2>
             </div>
             <div className="form-container-contacto">
-              <form className="row g-3 needs-validation" noValidate onSubmit={formik.handleSubmit}>
+              <form className="row g-3 needs-validation" noValidate onSubmit={handleSubmit}>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="rut" className="form-label"> RUT: </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="rut"
-                    name="rut"
-                    value={formik.values.rut}
-                    onChange={(e) => formik.setFieldValue('rut', format.rut(e.target.value))}
-                    placeholder="Ingresa tu RUT"
-                    required />
+                  <input type="text" className="form-control" id="rut" name="rut" value={formData.rut}
+                    onChange={handleChange} placeholder="Ingresa tu RUT" required />
                   <div className="invalid-feedback">Por favor, ingresa tu RUT.</div>
                 </div>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="nombreUnidad" className="form-label"> Nombre de la Unidad: </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="nombreUnidad"
-                    name="nombreUnidad"
-                    value={formik.values.nombreUnidad}
-                    onChange={formik.handleChange}
-                    placeholder="Ingresa el nombre de la Unidad"
-                    required />
+                  <input type="text" className="form-control" id="nombreUnidad" name="nombreUnidad"
+                    value={formData.nombreUnidad} onChange={handleChange} placeholder="Ingresa el nombre de la Unidad" required />
                   <div className="invalid-feedback"> Por favor, ingresa el nombre de la Unidad. </div>
                 </div>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="idRegion" className="form-label"> Región: </label>
-                  <select
-                    className="form-select"
-                    id="idRegion"
-                    name="idRegion"
-                    value={formik.values.idRegion}
-                    onChange={(e) => {
-                      formik.handleChange(e);
-                      dispatch(fetchComunasByRegionId(e.target.value));
-                    }}
-                    required
-                  >
+                  <select className="form-select" id="idRegion" name="idRegion" value={formData.idRegion}
+                    onChange={handleRegionChange} required>
                     <option value="" disabled> Selecciona una región </option>
-                    {regiones && regiones.map((region) => (
-                      <option key={region.id} value={region.id}> {region.nombre}</option>
-                    ))}
+                    {regiones && regiones.map((region) => (<option key={region.id} value={region.id}> {region.nombre}
+                    </option>))}
                   </select>
                   <div className="invalid-feedback">Por favor, selecciona una región.</div>
                 </div>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="idComuna" className="form-label"> Comuna: </label>
-                  <select
-                    className="form-select"
-                    id="idComuna"
-                    name="idComuna"
-                    value={formik.values.idComuna}
-                    onChange={formik.handleChange}
-                    required
-                  >
+                  <select className="form-select" id="idComuna" name="idComuna" value={formData.idComuna}
+                    onChange={handleComunaChange} required>
                     <option value="" disabled> Selecciona una comuna </option>
-                    {comunas && comunas.map((comuna) => (
-                      <option key={comuna.id} value={comuna.id}> {comuna.nombre}</option>
-                    ))}
+                    {comunas && comunas.map((comuna) => (<option key={comuna.id} value={comuna.id}> {comuna.nombre}
+                    </option>))}
                   </select>
                   <div className="invalid-feedback">Por favor, selecciona una comuna.</div>
                 </div>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="calle" className="form-label"> Calle: </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="calle"
-                    name="calle"
-                    value={formik.values.calle}
-                    onChange={formik.handleChange}
-                    placeholder="Ingresa la calle"
-                    required />
+                  <input type="text" className="form-control" id="calle" name="calle" value={formData.calle}
+                    onChange={handleChange} placeholder="Ingresa la calle" required />
                   <div className="invalid-feedback">Por favor, ingresa la calle.</div>
                 </div>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="numero" className="form-label"> Número: </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="numero"
-                    name="numero"
-                    value={formik.values.numero}
-                    onChange={formik.handleChange}
-                    placeholder="Ingresa el número"
-                    required />
+                  <input type="text" className="form-control" id="numero" name="numero" value={formData.numero}
+                    onChange={handleChange} placeholder="Ingresa el número" required />
                   <div className="invalid-feedback">Por favor, ingresa el número.</div>
                 </div>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="deptoCasa" className="form-label"> Depto/Casa: </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="deptoCasa"
-                    name="deptoCasa"
-                    value={formik.values.deptoCasa}
-                    onChange={formik.handleChange}
-                    placeholder="Ingresa el departamento/casa"
-                    required />
+                  <input type="text" className="form-control" id="deptoCasa" name="deptoCasa"
+                    value={formData.deptoCasa} onChange={handleChange} placeholder="Ingresa el departamento/casa" required />
                   <div className="invalid-feedback"> Por favor, ingresa el departamento/casa. </div>
                 </div>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="email" className="form-label"> Correo Electrónico: </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email" name="email"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    placeholder="Ingresa tu correo electrónico"
-                    required />
+                  <input type="email" className="form-control" id="email" name="email"
+                    value={formData.email} onChange={handleChange} placeholder="Ingresa tu correo electrónico" required />
                   <div className="invalid-feedback"> Por favor, ingresa un correo electrónico válido. </div>
                 </div>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="nombre" className="form-label"> Nombre: </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="nombre"
-                    name="nombre"
-                    value={formik.values.nombre}
-                    onChange={formik.handleChange}
-                    placeholder="Ingresa tu nombre"
-                    required />
+                  <input type="text" className="form-control" id="nombre" name="nombre"
+                    value={formData.nombre} onChange={handleChange} placeholder="Ingresa tu nombre" required />
                   <div className="invalid-feedback">Por favor, ingresa tu nombre.</div>
                 </div>
                 <div className="col-md-3 mb-3">
                   <label htmlFor="contrasena" className="form-label"> Contraseña: </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="contrasena"
-                    name="contrasena"
-                    value={formik.values.contrasena}
-                    onChange={formik.handleChange}
-                    placeholder="Ingresa tu contraseña"
-                    required />
+                  <input type="password" className="form-control" id="contrasena" name="contrasena"
+                    value={formData.contrasena} onChange={handleChange} placeholder="Ingresa tu contraseña" required />
                   <div className="invalid-feedback">Por favor, ingresa tu contraseña.</div>
                 </div>
                 <div className="col-md-12 mt-2">
@@ -279,7 +241,7 @@ const RegistroForm = () => {
                     </button>
                     <button className="btn btn-primary" type="submit"> Registrarse </button>
                   </div>
-                </div>
+                  </div>
               </form>
             </div>
           </div>
