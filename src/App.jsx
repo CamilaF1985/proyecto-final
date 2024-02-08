6eff6544da5dff8b75acf64dc36728eccc486836
Camila Fabbroni <camila.fabbroni1985@gmail.com>
2024-02-08 15:18:23 -0300

    pagina de inicio con imagen de fondo

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import Modal from 'react-modal';
 import LoginForm from './components/LoginForm.jsx';
 import ContactForm from './components/ContactForm.jsx';
 import RegistroForm from './components/RegistroForm.jsx';
-import ImageCarousel from './components/ImageCarousel.jsx'; // Importar el componente del carrusel
+import ImageCarousel from './components/ImageCarousel.jsx';
 import logo from './assets/img/logo.png';
+import fondo1 from './assets/img/fondo1.png'; // Importa la imagen de fondo
 import loginImage from './assets/img/login.png';
 import contactoImage from './assets/img/contacto.png';
 import registroImage from './assets/img/registro.png';
@@ -41,96 +42,112 @@ const App = () => {
 
   // Estructura JSX para la vista principal de la aplicación
   return (
-    <div className="contenedor-inicio mt-2 mb-2 p-2">
-      <div className="row">
-        {/* Sección del logo */}
-        <div className="col-12 col-md-6 d-flex justify-content-center">
-          <img src={logo} alt="Logo" className="contenedor-logo img-fluid img-logo" />
-        </div>
+    <div className="contenedor-principal">
+      <div className="contenedor-inicio mt-2 mb-2 p-2" style={{ position: 'relative' }}>
+        {/* Pseudoelemento para desaturar la imagen de fondo */}
+        <div
+          className="imagen-fondo-desaturada"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: `url(${fondo1})`,
+            filter: 'opacity(0.2)',
+            zIndex: -1,
+          }}
+        ></div>
+        <div className="row">
+          {/* Sección del logo */}
+          <div className="col-12 col-md-6 d-flex justify-content-center">
+            <img src={logo} alt="Logo" className="contenedor-logo img-fluid img-logo" />
+          </div>
 
-        {/* Sección de imágenes y enlaces de acción */}
-        <div className="col-12 col-md-6 text-center fila-imagen-personalizada d-flex flex-wrap">
-          {/* Iconos y enlaces */}
-          <div className="col xs-6 col-md-4 mb-3">
-            <div
-              className="contenedor-imagen contenedor-imagen-debajo contenedor-imagen-primera"
-              onClick={() => openModalAndRedirect('/login')}
-              style={{ cursor: 'pointer' }}
-            >
-              <img src={loginImage} alt="Inicio de sesión" className="img-fluid" />
+          {/* Sección de imágenes y enlaces de acción */}
+          <div className="col-12 col-md-6 text-center fila-imagen-personalizada d-flex flex-wrap">
+            {/* Iconos y enlaces */}
+            <div className="col xs-6 col-md-4 mb-3">
+              <div
+                className="contenedor-imagen contenedor-imagen-debajo contenedor-imagen-primera"
+                onClick={() => openModalAndRedirect('/login')}
+                style={{ cursor: 'pointer' }}
+              >
+                <img src={loginImage} alt="Inicio de sesión" className="img-fluid" />
+              </div>
+              <p className="texto-debajo-imagen">Iniciar sesión</p>
             </div>
-            <p className="texto-debajo-imagen">Iniciar sesión</p>
-          </div>
-          <div className="col xs-6 col-md-4 mb-3">
-            <div
-              className="contenedor-imagen contenedor-imagen-debajo"
-              onClick={() => openModalAndRedirect('/contacto')}
-              style={{ cursor: 'pointer' }}
-            >
-              <img src={contactoImage} alt="Contacto" className="img-fluid" />
+            <div className="col xs-6 col-md-4 mb-3">
+              <div
+                className="contenedor-imagen contenedor-imagen-debajo"
+                onClick={() => openModalAndRedirect('/contacto')}
+                style={{ cursor: 'pointer' }}
+              >
+                <img src={contactoImage} alt="Contacto" className="img-fluid" />
+              </div>
+              <p className="texto-debajo-imagen">Contáctanos</p>
             </div>
-            <p className="texto-debajo-imagen">Contáctanos</p>
-          </div>
-          <div className="col xs-12 col-md-4 mb-3">
-            <div
-              className="contenedor-imagen contenedor-imagen-debajo contenedor-registro"
-              onClick={() => openModalAndRedirect('/registro')}
-              style={{ cursor: 'pointer' }}
-            >
-              <img src={registroImage} alt="Registro" className="img-fluid" />
+            <div className="col xs-12 col-md-4 mb-3">
+              <div
+                className="contenedor-imagen contenedor-imagen-debajo contenedor-registro"
+                onClick={() => openModalAndRedirect('/registro')}
+                style={{ cursor: 'pointer' }}
+              >
+                <img src={registroImage} alt="Registro" className="img-fluid" />
+              </div>
+              <p className="texto-debajo-imagen">Regístrate</p>
             </div>
-            <p className="texto-debajo-imagen">Regístrate</p>
+          </div>
+
+          {/* Carrusel de imágenes */}
+          <div className="col xs-12 col-md-12 mb-3 d-flex justify-content-center">
+            <ImageCarousel />
+          </div>
+
+          {/* Contenedor "Quiénes somos" y "Nuestra Ubicación" */}
+          <div className="row justify-content-center ms-1 mb-3">
+            {/* Quiénes somos */}
+            <div className="col-xs-12 col-md-5 quienes-somos-contenedor text-justify">
+              <h2 className="quienes-somos-titulo">Quiénes somos</h2>
+              <MaquinaEscribir />
+            </div>
+
+            {/* Mapa de Google con encabezado */}
+            <div className="col-12 col-md-5 google-maps-contenedor text-justify">
+              <h2 className="ubicacion-titulo">Nuestra Ubicación</h2>
+              <Mapa />
+            </div>
           </div>
         </div>
 
-        {/* Carrusel de imágenes */}
-        <div className="col xs-12 col-md-12 mb-3 d-flex justify-content-center">
-          <ImageCarousel />
-        </div>
-
-        {/* Contenedor "Quiénes somos" y "Nuestra Ubicación" */}
-        <div className="row justify-content-center ms-1 mb-3">
-          {/* Quiénes somos */}
-          <div className="col-xs-12 col-md-5 quienes-somos-contenedor text-justify">
-            <h2 className="quienes-somos-titulo">Quiénes somos</h2>
-            <MaquinaEscribir />
-          </div>
-
-          {/* Mapa de Google con encabezado */}
-          <div className="col-12 col-md-5 google-maps-contenedor text-justify">
-            <h2 className="ubicacion-titulo">Nuestra Ubicación</h2>
-            <Mapa />
-          </div>
-        </div>
+        {/* Modal utilizando ReactDOM.createPortal */}
+        {ReactDOM.createPortal(
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={handleCloseModal}
+            contentLabel={location.pathname === '/login' ? 'LoginForm Modal' : 'ContactForm Modal'}
+            className="modal-content"
+            overlayClassName="modal-overlay"
+          >
+            {/* Contenido del modal (LoginForm, ContactForm, o RegistroForm según la ruta) */}
+            {location.pathname === '/login' ? (
+              <LoginForm />
+            ) : location.pathname === '/contacto' ? (
+              <ContactForm />
+            ) : location.pathname === '/registro' ? (
+              <RegistroForm />
+            ) : null}
+          </Modal>,
+          document.body
+        )}
       </div>
-
-      {/* Modal utilizando ReactDOM.createPortal */}
-      {ReactDOM.createPortal(
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={handleCloseModal}
-          contentLabel={location.pathname === '/login' ? 'LoginForm Modal' : 'ContactForm Modal'}
-          className="modal-content"
-          overlayClassName="modal-overlay"
-        >
-          {/* Contenido del modal (LoginForm, ContactForm, o RegistroForm según la ruta) */}
-          {location.pathname === '/login' ? (
-            <LoginForm />
-          ) : location.pathname === '/contacto' ? (
-            <ContactForm />
-          ) : location.pathname === '/registro' ? (
-            <RegistroForm />
-          ) : null}
-
-        </Modal>,
-        document.body
-      )}
     </div>
   );
 };
 
 // Exportar el componente principal de la aplicación
 export default App;
+
 
 
 
